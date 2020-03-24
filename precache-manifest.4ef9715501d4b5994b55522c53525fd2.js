6443f2bb429c9dd754a5c1946c85998a7b781a08
kamper541 <kamper543@gmail.com>
2020-03-25 00:20:00 +0700

    Updates

---
+++ precache-manifest.4ef9715501d4b5994b55522c53525fd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd68a5b276d195137d34c51bd0405fcb",
+    "url": "/lab8-deploy/index.html"
+  },
+  {
+    "revision": "40ea57a2365c449a5a3d",
+    "url": "/lab8-deploy/static/css/main.04418a4e.chunk.css"
+  },
+  {
+    "revision": "377c7ad5190137d7e3bc",
+    "url": "/lab8-deploy/static/js/2.5268e503.chunk.js"
+  },
+  {
+    "revision": "088762ca49c61ec7ee3934bedd4dacda",
+    "url": "/lab8-deploy/static/js/2.5268e503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ea57a2365c449a5a3d",
+    "url": "/lab8-deploy/static/js/main.78207468.chunk.js"
+  },
+  {
+    "revision": "70a40cef67a674332b14",
+    "url": "/lab8-deploy/static/js/runtime-main.55665120.js"
+  }
+]);
